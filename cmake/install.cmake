ed23c91ad816904b66b43ec05d3368384ca2bbcb
Dmitry Zheltkov <dmitry.zheltkov@gmail.com>
2023-10-25 09:44:39 +0300

    Changed PROJECT_BINAY_DIR to CMAKE_CURRENT_BINARY_DIR where necessary

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -1,10 +1,10 @@
 include(GNUInstallDirs)
 target_include_directories(cxxblas PUBLIC $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/include>
-                                          $<BUILD_INTERFACE:${PROJECT_BINARY_DIR}/include>
+                                          $<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}/include>
                                           $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>)
 
 install(DIRECTORY "${PROJECT_SOURCE_DIR}/include/" DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
-install(DIRECTORY "${PROJECT_BINARY_DIR}/include/" DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
+install(DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/include/" DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
 
 install(
         TARGETS cxxblas
@@ -21,14 +21,14 @@ install(
 include(CMakePackageConfigHelpers)
 
 write_basic_package_version_file(
-                                 CXXBLASConfigVersion.cmake
+                                 ${CMAKE_CURRENT_BINARY_DIR}/CXXBLASConfigVersion.cmake
                                  VERSION ${PROJECT_VERSION}
                                  COMPATIBILITY SameMinorVersion)
 
 configure_package_config_file(
-                              "${PROJECT_SOURCE_DIR}/cmake/CXXBLASConfig.cmake.in" CXXBLASConfig.cmake
+                              "${PROJECT_SOURCE_DIR}/cmake/CXXBLASConfig.cmake.in" ${CMAKE_CURRENT_BINARY_DIR}/CXXBLASConfig.cmake
                               INSTALL_DESTINATION ${CXXBLAS_CMAKE_PACKAGE_INSTALL_SUBDIR})
 
-install(FILES "${PROJECT_BINARY_DIR}/CXXBLASConfig.cmake"
-              "${PROJECT_BINARY_DIR}/CXXBLASConfigVersion.cmake"
+install(FILES "${CMAKE_CURRENT_BINARY_DIR}//CXXBLASConfig.cmake"
+              "${CMAKE_CURRENT_BINARY_DIR}//CXXBLASConfigVersion.cmake"
         DESTINATION ${CXXBLAS_CMAKE_PACKAGE_INSTALL_SUBDIR})
